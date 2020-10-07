51ddf428906f863ff802e5504340eccad8dd7e56
Jesse Savary <jesse-savary@users.noreply.github.com>
2020-10-07 19:32:02 -0300

    add zuliprc file support, remove unneeded dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,40 @@ import * as core from '@actions/core'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Zulip from 'zulip-js'
+import Path from 'path'
+import FS from 'fs'
+
+// By default, the action is given an input for the zuliprc parameter
+// However, the workflow can input 'false' which should explicitly
+// disable any zuliprc-related functionality
+async function get_path_to_zulip_rc(): Promise<string | undefined> {
+  const zuliprc_input = core.getInput('zuliprc')
+
+  // allow disabling of zuliprc file parsing
+  if (zuliprc_input === 'false') return undefined
+
+  // resolve the full path to the file and validate that it exists and we can read from it
+  return new Promise(resolve => {
+    const resolved_path = Path.resolve(zuliprc_input)
+    core.debug(`Full path to zuliprc file: ${resolved_path}`)
+    FS.access(resolved_path, FS.constants.F_OK | FS.constants.R_OK, error => {
+      core.debug(`Result of zuliprc file access check: ${error ? error : 'OK'}`)
+
+      if (error) resolve(undefined)
+      else resolve(resolved_path)
+    })
+  })
+}
 
 async function run(): Promise<void> {
   core.debug('Executing run')
 
+  const path_to_zuliprc = await get_path_to_zulip_rc()
+
   // assemble our configuration before before awaiting anything to catch early input errors
   const config = {
     client: {
+      zuliprc: path_to_zuliprc,
       username: core.getInput('username'),
       apiKey: core.getInput('api-key'),
       realm: core.getInput('url')
