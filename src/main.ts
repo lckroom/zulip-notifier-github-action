c98ce463618ff666cd02183a102476c5b045910b
Jesse Savary <jesse-savary@users.noreply.github.com>
2020-10-07 17:10:22 -0300

    improve error handling, code quality for main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,30 @@
 import * as core from '@actions/core'
+// FIXME create types for zulip-js
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Zulip from 'zulip-js'
 
 async function run(): Promise<void> {
-  try {
-    const client_config = {
+  // assemble our configuration before before awaiting anything to catch early input errors
+  const config = {
+    client: {
       username: core.getInput('username'),
       apiKey: core.getInput('api-key'),
       realm: core.getInput('url')
-    }
-
-    const message_config = {
+    },
+    message: {
       type: 'stream',
       to: core.getInput('stream'),
       topic: core.getInput('topic'),
       content: core.getInput('message')
     }
-
-    const client = await Zulip(client_config)
-
-    const result = await client.messages.send(message_config)
-    core.info(result)
-  } catch (error) {
-    core.setFailed(error.message)
   }
+
+  const client = await Zulip(config.client)
+  const result = await client.messages.send(config.message)
+  core.info(result)
 }
 
-run()
+// `catch` used in this way is safer/more robust than wrapping the contents of `run` in a try-catch
+// eslint-disable-next-line github/no-then
+run().catch(core.setFailed)
